a4738c012676c7b196745032b53bc66c2a45aa66
김종혁 <zxxd23@naver.com>
2021-07-15 03:09:37 +0900

    주석달았습니다

--- Crypt_of_Necrodancer_Copy/randomMap.cpp
+++ Crypt_of_Necrodancer_Copy/randomMap.cpp
@@ -156,53 +156,54 @@ void randomMap::render()
 
 void randomMap::generate()
 {
-	setTile();
-	makeRooms();
-	makePassage();
-	removeUnusedWalls();
+	setTile();//일단모든타일을 바닥과 벽을칠해놓는다
+	makeRooms(); // 방크기 정하고  정했다면 벽삭제해서 방을 만들어준다 
+				// 사이즈가 7x7이라면 방크기는 5X5
+	makePassage();//방과 방을 연결해준다
+	removeUnusedWalls(); //연결하고 안쓰는 곳을 모든 벽,바닥을 삭제해준다
 }
 
 void randomMap::makeRooms()
 {
-	m_room.clear();
+	m_room.clear(); //방정보를 담을 백터 클리어
 	
-	int gridwidth = TILEX / gridX;
-	int gridHeight = TILEY / gridY;
+	int gridwidth = TILEX / gridX; // 전체 타일을  총 몇구역으로 나눌껀지
+	int gridHeight = TILEY / gridY; // 전체 타일을 총 몇구역으로 나눌껀지
 
 	for (int y = 0; y < gridY; ++y)
 	{
 		for (int x = 0; x < gridX; ++x)
 		{
-			int width = RND->getFromIntTo(roomWidthMin, gridwidth);
+			int width = RND->getFromIntTo(roomWidthMin, gridwidth); // 최소 방크기에서 총제한구역으로 랜덤값생성
 			int height = RND->getFromIntTo(roomHeightMin, gridHeight);
 
-			if (RND->getInt(gridX*gridY) == 0)
+			if (RND->getInt(gridX*gridY) == 0) //다생성하면 재미없으니까 조그만한 방도 만들어놓는다
 			{
 				width = height = 4;
 			}
 
-			int left = x * (gridwidth + 1) + RND->getInt(gridwidth - width);
-			int top = y * (gridHeight + 1) + RND->getInt(gridHeight - height);
+			int left = x * (gridwidth + 1) + RND->getInt(gridwidth - width); // 각방들의 첫x좌표 
+			int top = y * (gridHeight + 1) + RND->getInt(gridHeight - height);//각방들의 첫y좌표
 
-			ROOM room;
-			room.left = left;
-			room.top = top;
-			room.right = left + width;
-			room.bottom = top + height;
-			room.width = width;
-			room.height = height;
+			ROOM room; //방정보 구조체
+			room.left = left; //값
+			room.top = top; //다
+			room.right = left + width;//넣
+			room.bottom = top + height;//어
+			room.width = width;//이건 사실 없어도됌;;
+			room.height = height; // 없어도됌;;
 
 			if (x > 0)
-				room.neighbors.emplace_back((x - 1) + y * gridX);
-			if (x < gridX - 1)
-				room.neighbors.emplace_back((x + 1) + y * gridX);
+				room.neighbors.emplace_back((x - 1) + y * gridX); // 근처에 방이 몇개있는지 체크하는 알고리즘
+			if (x < gridX - 1)   
+				room.neighbors.emplace_back((x + 1) + y * gridX); //구석이면1~2개 구석이아닐경우 2~3개
 			if (y > 0)
 				room.neighbors.emplace_back(x + (y - 1)*gridX);
 			if (y < gridY - 1)
 				room.neighbors.emplace_back(x + (y + 1)*gridX);
-			placeRoom(room);
-			m_room.emplace_back(room);
-
+			placeRoom(room); // 실제로 방만들어주는 구간
+			m_room.emplace_back(room); //방을 만들었으면 방정보담은 백터에 새롭게 추가해줍니다
+	
 		}
 	}
 
@@ -210,30 +211,30 @@ void randomMap::makeRooms()
 
 void randomMap::makePassage()
 {
-	vector<size_t> unconnected;
-	vector<size_t> connected;
+	vector<size_t> unconnected; //연결이안된방을 체크해주는 백터 기본적으로 방생성을하면 다 연결이안되어있으니까 여기에 모든방을 넣는다
+	vector<size_t> connected; //연결이 된방을 체크해주는 백터
 	for (size_t i = 0; i < m_room.size(); ++i)
-		unconnected.emplace_back(i);
-		//셔플부터
+		unconnected.emplace_back(i); // 모든방을 일단 연결이안된방 백터에 넣는다
 
+	//어떤연결안된방을 처음에 뽑을지 셔플로 결정
 	shuffle(unconnected, unconnected.size());
-	connected.emplace_back(unconnected.back());
+	connected.emplace_back(unconnected.back());//그다음 맨마지막에 있는 무작위 방을 하나 뽑아서 넣는다. 
 
-	unconnected.pop_back();
-
-	while (!unconnected.empty())
+	unconnected.pop_back(); //그리고 연결안된방을 하나빼고
+	 
+	while (!unconnected.empty()) //모든방이 연결될때까지 돌려!
 	{
-		size_t i = connected[RND->getInt(connected.size())];
-		if (m_room[i].neighbors.empty())
+		size_t i = connected[RND->getInt(connected.size())]; // 랜덤으로 연결된방하나 인덱스를 뽑고
+		if (m_room[i].neighbors.empty())//예외처리를 해줍니다 인접한방이 없을 수 도있음
 		{
 			connected.erase(remove(connected.begin(), connected.end(), i), connected.end());
 			continue;
 		}
-		size_t j= m_room[i].neighbors[RND->getInt(m_room[i].neighbors.size())];
+		size_t j= m_room[i].neighbors[RND->getInt(m_room[i].neighbors.size())];//그 연결하려는방 하나중 인접한거 하나를뽑아요
 
-		connectRooms(i, j);
-		unconnected.erase(remove(unconnected.begin(), unconnected.end(), j), unconnected.end());
-		connected.emplace_back(j);
+		connectRooms(i, j); // 그다음 연결해줍니다.
+		unconnected.erase(remove(unconnected.begin(), unconnected.end(), j), unconnected.end());//연결이 끝나면 그 연결안된방을 안연결된백터에 지워주고
+		connected.emplace_back(j);//그 방을 연결되었다고 표시해줍니다.
 	}
 
 }
@@ -244,24 +245,24 @@ void randomMap::removeUnusedWalls()
 	{
 		for (int x = 0; x < TILEX; ++x)
 		{
-			int numWalls = 0;
+			int numWalls = 0; // 현재 탐색할 타일을 기준으로 주변에 쓰고있는 벽이있는지 탐색하기위한 변수
 
-			for (int dy = -1; dy <= 1; ++dy)
+			for (int dy = -1; dy <= 1; ++dy)//
 			{
-				for (int dx = -1; dx <= 1; ++dx)
+				for (int dx = -1; dx <= 1; ++dx)//예를들어 0,0을기준으로 -1,-1부터 1,1까지 9번탐색합니다.
 				{
 					if (x + dx < 0 || y + dy < 0 || x + dx >= TILEX || y + dy >= TILEY)
-						++numWalls;
+						++numWalls; //주변이 맵밖을 나가는외곽이다 하면 ++
 					else
 					{
 						const int tileNumber = _tiles[y + dy][x + dx].obj;
 						const int tileshit = _tiles[y + dy][x + dx].terrain;
-						if (tileNumber == WALL_BASIC || tileshit == EMPTY)
+						if (tileNumber == WALL_BASIC || tileshit == EMPTY) //근처에 벽이나 텅 비어있는바닥이있으면 ++
 							++numWalls;
 					}
 				}
 			}
-			if (numWalls == 9)
+			if (numWalls == 9)//주변이 벽이거나 텅빈바닥이있는 쓸모없는타일이야! 하면 그곳을 텅 비워줍니다.
 			{
 				_tiles[y][x].obj = OBJ_NONE;
 				_tiles[y][x].terrain = EMPTY;
@@ -276,75 +277,74 @@ void randomMap::placeRoom(const ROOM & room)
 	{
 		for (int x = room.left + 1; x < room.right - 1 ;++x)
 		{
-			_tiles[y][x].obj = OBJ_NONE;
+			_tiles[y][x].obj = OBJ_NONE; // 벽삭제하고 방만들어줌
 		}
 	}
 }
 
 void randomMap::connectRooms(size_t i, size_t j)
 {
-	if (j < i)
-		swap(i, j);
+	if (j < i) // i는 연결출발점 j는 연결도착점이니까 출발점이 도착점보다 크다면
+		swap(i, j); // 바꿔줍시다
 
-	ROOM& from = m_room[i];
-	ROOM& to = m_room[j];
+	ROOM& from = m_room[i]; //출발할 방
+	ROOM& to = m_room[j]; // 도착할방
 
-	if (j - i == 1) //Horizontal passage
+	if (j - i == 1) //Horizontal passage 옆으로 이동
 	{
-		const int fromX = from.right - 1;
-		//((from.top+1) -(from.bottom-2)==0) ? from.top+1 :(from.top +1, from.bottom -2)
-		const int fromY =  RND->getFromIntTo(from.top+1 , from.bottom -2);
+		const int fromX = from.right - 1; // 오른쪽으로 출발하기때문에 
+										 //출발할방의 바로 오른쪽 벽 한칸 뒤 x좌표가 시작점.
+		const int fromY =  RND->getFromIntTo(from.top+1 , from.bottom -2); // 그 시작점의 y좌표는 랜덤으로 갖는다. 
 
-		const int toX = to.left+1;
-		//((to.top + 1) - (to.bottom - 2)==0) ? to.top +1 : (to.top+1 , to.bottom-2)
-		const int toY = RND->getFromIntTo(to.top+1 , to.bottom-2);
+		const int toX = to.left+1; // 도착할곳이 왼쪽으로 오기때문에 
+									//도착할방의 바로 벽한칸 앞쪽 x좌표가 도착점.
+		const int toY = RND->getFromIntTo(to.top+1 , to.bottom-2); //1자로 생기면 재미없으니까 여기도 랜덤하게 y좌표를 뽑는다.
 
-		//((fromX+1)-(toX-1)==0)? fromX+1 : (fromX +1, toX -1)
-		const int centerX = RND->getFromIntTo(fromX +1, toX -1);
-		const int dy = toY > fromY ? 1 : -1;
+	
+		const int centerX = RND->getFromIntTo(fromX +1, toX -1); //서로다른Y좌표로 이동할 중점X좌표을 구함
+		const int dy = toY > fromY ? 1 : -1; //그리고 도착점이 시작점보다 y좌표가 크면 +1씩 해줘야되고,
+											//도착점이 시작점보다 y좌표가 작으면 -1씩해주어서 도착점에 y좌표를 맞춰야한다.
 
-		for (int x = fromX; x < centerX; ++x)
+		for (int x = fromX; x < centerX; ++x) //출발점에서 y좌표를 바꿔야할 중점X좌표까지 오른쪽으로 이동 
 		{
 			_tiles[fromY][x].obj = OBJ_NONE;
 		}
-		for (int y = fromY; y != toY; y += dy)
-		{
-			_tiles[y][centerX].obj = OBJ_NONE;
+		for (int y = fromY; y != toY; y += dy) // 만약에 출발점과 도착점의 Y좌표가 같다면 무시 
+		{										//아니라면 도착점에 맞춰 +1,-1씩해서 길을 만든다
+			_tiles[y][centerX].obj = OBJ_NONE; 
 		}
-		for (int x = centerX; x <= toX; ++x)
+		for (int x = centerX; x <= toX; ++x) // 중점X좌표를 경유했다면 이제 도착점까지 오른쪽으로이동
 		{	
 			_tiles[toY][x].obj = OBJ_NONE;
 		}
 
 	}
-	else // Vertical passage
+	else // Vertical passage //위아래로이동
 	{
-		//((from.left +1)-(from.right-2)==0)? from.left+1: 
-		const int fromX = RND->getFromIntTo(from.left +1 , from.right-2 );
-		const int fromY = from.bottom - 1;
+		const int fromX = RND->getFromIntTo(from.left +1 , from.right-2 ); //랜덤으로 출발점의 X좌표를 뽑는다. 
+		const int fromY = from.bottom - 1; //아래쪽으로 이동하기때문에 출발방의 밑벽 한칸앞이 출발Y좌표.
 
-		//((to.left+1) - (to.right-2)==0)? to.left+1: 
-		const int toX = RND->getFromIntTo(to.left +1, to.right -2);
-		const int toY = to.top+1;
+		const int toX = RND->getFromIntTo(to.left +1, to.right -2); // 도착점도 마찬가지로 1직선으로 오면 재미없으니까 랜덤으로 X좌표 뽑아줌
+		const int toY = to.top+1; //위쪽으로 출발점이 오기때문에 도착방 윗벽의 한칸 밑이 도착 Y좌표 
 
-		// ((fromY +1) - (toY-1)==0)? fromY+1:
-		const int centerY = RND->getFromIntTo(fromY+1, toY -1);
-		const int dx = toX > fromX ? 1 : -1;
-		for (int y = fromY; y < centerY; ++y)
+
+		const int centerY = RND->getFromIntTo(fromY+1, toY -1); // 마찬가지로 X좌표를 바꿔야할 경유Y좌표를 구한다.
+		const int dx = toX > fromX ? 1 : -1; // 도착점 X좌표가 출발점 X좌표보다 크다면 +1, 아니면 -1이다.
+		for (int y = fromY; y < centerY; ++y) //출발점에서 X좌표를 바꿔야할 중점Y좌표까지 아래로 이동 
 		{
 			_tiles[y][fromX].obj = OBJ_NONE;
 		}
-		for (int x = fromX; x != toX; x += dx)
-		{
+		for (int x = fromX; x != toX; x += dx)// 만약에 출발점과 도착점의 X좌표가 같다면 무시
+		{									  //아니라면 도착점에 맞춰 +1,-1씩해서 길을 만든다
 			_tiles[centerY][x].obj = OBJ_NONE;
 		}
-		for (int y = centerY; y <= toY; ++y)
+		for (int y = centerY; y <= toY; ++y)// 중점Y좌표를 경유했다면 이제 도착점까지 아래로이동
 		{
 			_tiles[y][toX].obj = OBJ_NONE;
 		}
 	}
 
-	from.neighbors.erase(remove(from.neighbors.begin(), from.neighbors.end(), j), from.neighbors.end());
+	from.neighbors.erase(remove(from.neighbors.begin(), from.neighbors.end(), j), from.neighbors.end()); // 연결이 다됬다면 인접했던 각자방번호를 삭제해줍니다
 	to.neighbors.erase(remove(to.neighbors.begin(), to.neighbors.end(), i), to.neighbors.end());
 
 }
