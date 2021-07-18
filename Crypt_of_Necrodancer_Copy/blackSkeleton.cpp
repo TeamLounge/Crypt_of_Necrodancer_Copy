a64d85fa544b6ad59db2a8edffeaa084d1489211
김종혁 <zxxd23@naver.com>
2021-07-19 01:23:45 +0900

    소스트리이상해서 나눠져서 올림;

---
+++ Crypt_of_Necrodancer_Copy/blackSkeleton.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "blackSkeleton.h"
+
+HRESULT blackSkeleton::init()
+{
+	return S_OK;
+}
+
+void blackSkeleton::update()
+{
+
+}
+
+void blackSkeleton::release()
+{
+}
+
+void blackSkeleton::render()
+{
+}
