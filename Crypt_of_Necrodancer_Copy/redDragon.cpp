6baad0bd10560e3cfea2f30c65a9e1ccebaee5ff
정무현 <procendp@gmail.com>
2021-07-28 11:10:47 +0900

    레드 드래곤 움직임, 프레임 수정중.. 이미지 렌더순서도 가즈아!!!

--- Crypt_of_Necrodancer_Copy/redDragon.cpp
+++ Crypt_of_Necrodancer_Copy/redDragon.cpp
@@ -3,99 +3,263 @@
 
 HRESULT redDragon::init(int playerIndexX, int playerIndexY)
 {
-	//_toRender = _damageRender = false;
-	//_direction = LEFT;	//왼쪽 보고 태어나
-	//_frameCount = 0;
-	//_frameIndex = 0;
+	minotaurAndDragon::init(playerIndexX, playerIndexY);		// == skeleton.cpp -> init() 전체
 
-	//_isMove = false;		//시작하자마자 움직여
-	//_isTime = false;
 
-	//_gravity = 0;
+	//redDragon class 독립적인 선언 
+	_renderTime = _movingTime = TIMEMANAGER->getWorldTime();
+	_hp = 6;
+	_img = IMAGEMANAGER->findImage("dragonRed");
+	_img->setFrameY(0);
 
-	//_worldTime = TIMEMANAGER->getWorldTime();
-
-	////맵에 뿌려주기
-	//setArrangement();	//타일에 먼저 뿌리자
-
-	//_img = IMAGEMANAGER->findImage("redDragon");	//자식
-	//_rc = _map->getRect(_tileX, _tileY);
-	//_map->setIsEnemy(_tileX, _tileY, true);
-	//_x = _rc.left;
-	//_y = _rc.top - (_rc.bottom - _rc.top) / 2;
-
+	isSameLine = false;	//플레이어와 동일 선 상에 있는지 확인하고자하는 불값	//레드드래곤 클래스 안에서 선언
 
 	return S_OK;
 }
 
+void redDragon::release()
+{
+}
+
 void redDragon::update(int playerIndexX, int playerIndexY)
 {
-	setRedDragonFrame();
+	minotaurAndDragon::update(playerIndexX, playerIndexY);
+
+	if (isFind && toRender)
+	{
+		if (_index >= 4)
+		{
+			_index = 0;
+		}
+		else
+		{
+			_index = 4;
+		}
+		toRender = false;
+	}
+	
+	setRedDragonFrame();	//_count 증가시키고, _index 받아서 프레임 조절
 	moveRedDragon();
+
+	//if (_index <= 3) {
+	//	_count++;
+	//	if (_count % 10 == 0)
+	//	{
+	//		_index++;
+	//		if (_index > 3) _index = 0;
+	//		_count = 0;
+	//	}
+	//}
+	//if (isAction && _index >= 5)
+	//{
+	//	if (isTime)
+	//	{
+	//		_index++;
+	//	}
+	//	if (_index > 8)
+	//	{
+	//		_index = 0;
+	//		isAction = false;
+	//		_astar->callPathFinder(_tilex, _tiley);
+	//	}
+	//}
+	//if (!isAction&&isFind)
+	//{
+	//	//액션
+	//	if (_tilex == playerIndexX)
+	//	{
+	//		_index == 4;
+	//		if (_tiley > playerIndexY)
+	//		{
+	//			isAction = true;
+	//			_index == 4;
+	//			_dir = UP;
+	//			_x = (_rc.left + _rc.right) / 2 - (_img->getFrameWidth() / 2);
+	//			_y = _rc.top - ((_rc.bottom - _rc.top) / 2) - (_img->getFrameHeight() / 2);
+	//		}
+	//		else if (_tiley < playerIndexY)
+	//		{
+	//			isAction = true;
+
+	//			_index == 4;
+	//			_dir = DOWN;
+	//			_x = (_rc.left + _rc.right) / 2 - (_img->getFrameWidth() / 2);
+	//			_y = _rc.top - ((_rc.bottom - _rc.top) / 2) - (_img->getFrameHeight() / 2);
+	//		}
+	//	}
+	//	else if (_tiley == playerIndexY)
+	//	{
+	//		if (_tilex > playerIndexX)
+	//		{
+	//			isAction = true;
+	//			_index == 4;
+	//			_dir = LEFT;
+	//			_x = (_rc.left + _rc.right) / 2 - (_img->getFrameWidth() / 2);
+	//			_y = _rc.top - ((_rc.bottom - _rc.top) / 2) - (_img->getFrameHeight() / 2);
+	//		}
+	//		else if (_tilex < playerIndexX)
+	//		{
+	//			isAction = true;
+	//			_index == 4;
+	//			_dir = RIGHT;
+	//			_x = (_rc.left + _rc.right) / 2 - (_img->getFrameWidth() / 2);
+	//			_y = _rc.top - ((_rc.bottom - _rc.top) / 2) - (_img->getFrameHeight() / 2);
+	//		}
+	//	}
+	//}
+	////moveMotion();
+
+	if (playerIndexX < _tilex)
+	{
+		_indey = 0;
+	}
+	else
+	{
+		_indey = 1;
+	}
+	isTime = false;
+	
+	
+	
 }
 
 void redDragon::render()
 {
-	/*if (KEYMANAGER->isToggleKey(VK_TAB))
+	minotaurAndDragon::render();
+
+	//알파렌더
+	if (_map->getAlpha(_tilex, _tiley) <= 255 && _map->getAlpha(_tilex, _tiley) > 150)
 	{
-		Rectangle(getMemDC(), _rc);
+		_img = IMAGEMANAGER->findImage("dragonRed");
+		_img->frameRender(getMemDC(), _x - 60, _y - 55, _index, _indey);		//이미지 위치 잡아주기 위해 임의 값 감소시켜줌
 	}
+	else if (_map->getAlpha(_tilex, _tiley) <= 150 && _map->getAlpha(_tilex, _tiley) > 0)
+	{
+		_img = IMAGEMANAGER->findImage("dragonRed_dark");
+		_img->frameRender(getMemDC(), _x - 60, _y - 55, _index, _indey);
+	}
+	//_img->frameRender(getMemDC(), _x, _y, _index, _indey);
 
-	_img->frameRender(getMemDC(), _x, _y, _currentFrameX, _currentFrameY);*/
-}
-
-void redDragon::setArrangement()
-{
-	////랜덤 배치
-	//while (true)
-	//{
-	//	//랜덤방에 배치
-	//	int random = RND->getInt(_map->getRoom().size());
-	//	if (_map->getRoom()[random].roomState == ROOM_START ||
-	//		_map->getRoom()[random].roomState == ROOM_SHOP)
-	//		continue;	// 그 방이 플레이어방이거나 상점이면 컨티뉴
-
-	//	//랜덤하게 찍은 방안의 좌표 중 벽이 있어도 컨티뉴
-	//	_tileX = RND->getFromIntTo(_map->getRoom()[random].x, _map->getRoom()[random].x + _map->getRoom()[random].width);
-	//	_tileY = RND->getFromIntTo(_map->getRoom()[random].y, _map->getRoom()[random].y + _map->getRoom()[random].height);
-	//	if (_map->getTileObject(_tileX, _tileY) != OBJ_NONE || _map->getTileTerrain(_tileX, _tileY) != DIRT1
-	//		|| _map->getIsEnemy(_tileX, _tileY))		//복도 tile은 2지만, room으로 배치하니까 무관!
-	//		continue;
-	//	break;		//// 모든 컨티뉴 지옥에서 벗어낫다면 빠져 나오기
-	//}
+	char str[128];
+	sprintf_s(str, "hp = %d", _hp);
+	TextOut(getMemDC(), _rc.left, _rc.top, str, strlen(str));
 }
 
 void redDragon::setRedDragonFrame()
 {
-	//if (TIMEMANAGER->getWorldTime() - _worldTime > 0.5f)
-	//{
-	//	_worldTime = TIMEMANAGER->getWorldTime();
-	//}
+	//드래곤과 플레이어가 같은 줄이면
+	if (_tiley == _playerindey)
+	{
+		isSameLine = true;
+	}
+	//드래곤과 플레이어가 같은 줄이 아니면
+	else isSameLine = false;
 
-	//_frameCount++;
-	//if (_frameCount % 8 == 0)
-	//{
-	//	_frameCount = 0;
-	//	if (_currentFrameX >= IMAGEMANAGER->findImage("redDragon")->getMaxFrameX())
-	//	{
-	//		_currentFrameX = 0;
-	//	}
-	//	else
-	//	{
-	//		_currentFrameX++;
-	//	}
-	//	//방향 바꿔 보여주기
-	//	if (_direction == LEFT)
-	//	{
-	//		_currentFrameY = 0;
-	//	}
-	//	else if (_direction == RIGHT)
-	//	{
-	//		_currentFrameY = 1;
-	//	}
-	//}
+	//_count++;
+	if (isSameLine)
+	{
+		_count++;
+		_index = 2;
+		_index++;
+		if (_index >= 2 && _index <= 6)	//2 - 6		//광선 쏘기
+		{
+			if (_count % 5 == 0)
+			{
+				_index++;
+				if (_index > 6) _index = 2;
+				_count = 0;
+			}
+		}
+		
+	}
+	else
+	{
+		_count++;
+		_index = 0;
+		if (_index >= 0 && _index <= 1)	//0, 1		//평상시 이동
+		{
+			if (_count % 10 == 0)
+			{
+				_index++;
+				if (_index > 1) _index = 0;
+				_count = 0;
+			}
+		}
+	}
 }
 
 void redDragon::moveRedDragon()
 {
+
+	//점프 이동 => 화이트 스켈레톤의 업데이트
+	if (isMove)
+	{
+		switch (_dir)
+		{
+		case NONE:
+			_gravity += 0.965f;
+			_y += -sinf(7 * PI / 9) * 9 + _gravity;
+			if (_y >= _rc.top - (_rc.bottom - _rc.top) / 2)
+			{
+				_y = _rc.top - (_rc.bottom - _rc.top) / 2;
+				isMove = false;
+				_gravity = 0;
+			}
+			break;
+
+		case LEFT:
+			_gravity += 0.965f;
+			_x += cosf(7 * PI / 9) * 9;
+			_y += -sinf(7 * PI / 9) * 9 + _gravity;
+			if (_x <= _rc.left)
+			{
+				_x = _rc.left;
+				isMove = false;
+				_gravity = 0;
+				if (_y >= (_rc.top - (_rc.bottom - _rc.top) / 2))
+				{
+					_y = _rc.top - (_rc.bottom - _rc.top) / 2;
+				}
+			}
+			break;
+
+		case RIGHT:
+			_gravity += 0.965f;
+			_x -= cosf(7 * PI / 9) * 9;
+			_y += -sinf(7 * PI / 9) * 9 + _gravity;
+			if (_x >= _rc.left)
+			{
+				_x = _rc.left;
+				isMove = false;
+				_gravity = 0;
+				if (_y >= (_rc.top - (_rc.bottom - _rc.top) / 2))
+				{
+					_y = _rc.top - (_rc.bottom - _rc.top) / 2;
+				}
+			}
+			break;
+
+		case UP:
+			_gravity += 0.2f;
+			_y += -sinf(PI / 2) * 9 + _gravity;
+			if (_y <= _rc.top - (_rc.bottom - _rc.top) / 2)
+			{
+				_y = _rc.top - (_rc.bottom - _rc.top) / 2;
+				isMove = false;
+				_gravity = 0;
+			}
+			break;
+
+		case DOWN:
+			_gravity += 1.2f;
+			_y += -sinf(PI / 2) + _gravity;
+			if (_y >= _rc.top - (_rc.bottom - _rc.top) / 2)
+			{
+				_y = _rc.top - (_rc.bottom - _rc.top) / 2;
+				isMove = false;
+				_gravity = 0;
+			}
+			break;
+		}
+	}
 }
